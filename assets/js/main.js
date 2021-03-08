b0b222139782002c566c203e0789f23a4f712d09
Giuditta <giudittamarino89@gmail.com>
2021-03-08 17:48:22 +0100

    seconda parte esercizio

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,5 @@
+// PRIMO PROBLEMA
+
 // CHIEDO ALL'UTENTE DI INSERIRE UNA PAROLA
 var parolaUtente = prompt ("Dammi una parola, ti dirò se è un palindromo:");
 
@@ -24,3 +26,55 @@ if (giocoPalindromi) {
 } else {
   risultatoDOM.innerHTML = "La tua parola non è un palindromo!"
 }
+
+// SECONDO PROBLEMA
+
+// FAR SCEGLIERE ALL'UTENTE PARI O DISPARI
+var paridispariUtente = prompt("Scrivi pari o dispari");
+
+var flag = false;
+
+if (paridispariUtente.toLowerCase() == "pari") {
+  var numeroUtente = prompt("Inserisci un numero da 1 a 5");
+  console.log(numeroUtente);
+  flag = true;
+} else if (paridispariUtente.toLowerCase() == "dispari") {
+  var numeroUtente = prompt("Inserisci un numero da 1 a 5");
+  console.log(numeroUtente);
+  console.log(flag);
+} else {
+  while (paridispariUtente.toLowerCase() != "pari" || paridispariUtente.toLowerCase() != "dispari") {
+    alert("non mi hai dato pari o dispari!");
+    var paridispariUtente = prompt("Scrivi pari o dispari");
+  }
+}
+
+// CREO UNA FUNZIONE PER GENERARE NUMERI RANDOM ENTRO UN RANGE
+function randomOneToFive(min, max){
+  var randomNumber = Math.floor(Math.random() * (max - min + 1) + min);
+  return randomNumber;
+}
+
+// GENERO UN NUMERO RANDOM DA 1 A 5 PER IL COMPUTER USANDO LA FUNZIONE
+var numeroComputer = randomOneToFive(1, 5);
+console.log(numeroComputer);
+
+// SOMMO IL  NUMERO FORNITO DALL'UTENTE E IL NUMERO GENERATO DAL PC E STABILISCO SE E' PARI O DISPARI TRAMITE UNA FUNZIONE
+var sommaNumeri = parseInt(numeroUtente) + parseInt(numeroComputer);
+console.log(sommaNumeri);
+
+function pariDispari(num) {
+  if (parseInt(!(num%2)) && (flag)) {
+    console.log("La somma è pari, hai vinto!");
+  } else if (parseInt(!(num%2)) && (!(flag)))  {
+    console.log("La somma è pari, hai perso!");
+  } else if (parseInt((num%2)) && (flag)) {
+    console.log("La somma è dispari, hai perso!");
+  } else if (parseInt((!(num%2))) && (!(flag))) {
+    console.log("La somma è dispari, hai vinto!");
+  }
+}
+
+var risultatoPariDispari = pariDispari(sommaNumeri);
+
+// STAMPO IL RISULTATO
